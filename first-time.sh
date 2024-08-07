38bb2cf7d492de5c4d5cb7d50c0f41fd8b49bed0
asancarc <adrian.sanchezcarchano@nttdata.com>
2024-08-07 09:55:21 +0200

    Feature: Update some info.

--- first-time.sh
+++ first-time.sh
@@ -8,7 +8,7 @@ docker-compose exec web composer install &&
 docker-compose exec web vendor/bin/run toolkit:build-dev &&
 docker-compose exec web vendor/bin/run toolkit:install-clean &&
 echo "########################################################################################"
-echo "Now, you have DRUPAL 10 with standard profile installed, you can check in localhost:8080" &&
+echo "Now, you have DRUPAL 11 with standard profile installed, you can check in localhost:8080" &&
 echo "########################################################################################" &&
 echo "Next line you can get a drush uli command result:" &&
 docker-compose exec web drush uli &&
